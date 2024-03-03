751e85716336f2d0d4d7d97fa8037b335257194b
Bruno Racobaldo Santos <60931988+racoba@users.noreply.github.com>
2024-03-03 13:32:25 -0300

    added server side props

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,30 @@
 "use client"
 import { AuthContext } from "@/context/AuthContext";
 import React, { useContext } from "react";
+import { parseCookies } from "nookies"
+import { GetServerSideProps } from "next";
 
 export default function Dashboard() {
     const { username } = useContext(AuthContext);
 
     return (
-        <h1>Dashboard de { username }</h1>
+        <h1>Dashboard de {username}</h1>
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    const { ["walletadmin.token"]: token } = parseCookies(ctx);
+
+    if (!token) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false
+            }
+        }
+    }
+
+    return {
+        props: {}
+    }
 }
