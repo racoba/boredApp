091f90adb5511d31846cc84041d1cccb6b7f8e4b
Bruno Racobaldo Santos <60931988+racoba@users.noreply.github.com>
2024-03-03 13:50:00 -0300

    added token validation on dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,30 +1,47 @@
 "use client"
 import { AuthContext } from "@/context/AuthContext";
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { parseCookies } from "nookies"
-import { GetServerSideProps } from "next";
+import { useRouter } from "next/navigation";
 
 export default function Dashboard() {
     const { username } = useContext(AuthContext);
+    const router = useRouter()
+    useEffect(()=>{
+        const { ["walletadmin.token"]: token } = parseCookies();
+
+        if(!token){
+            router.back();
+        }
+    },[])
 
     return (
         <h1>Dashboard de {username}</h1>
     )
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-    const { ["walletadmin.token"]: token } = parseCookies(ctx);
 
-    if (!token) {
-        return {
-            redirect: {
-                destination: "/",
-                permanent: false
-            }
-        }
-    }
 
-    return {
-        props: {}
-    }
-}
+
+
+
+
+
+// import { GetServerSideProps } from "next";
+
+// export const getServerSideProps: GetServerSideProps = async (ctx) => {
+//     const { ["walletadmin.token"]: token } = parseCookies(ctx);
+
+//     if (!token) {
+//         return {
+//             redirect: {
+//                 destination: "/",
+//                 permanent: false
+//             }
+//         }
+//     }
+
+//     return {
+//         props: {}
+//     }
+// }
